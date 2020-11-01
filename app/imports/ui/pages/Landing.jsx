a4e6b7aa97b1dedbe7e3e3ac651227aeb5df9eb0
Christian Pak <cpak2019@users.noreply.github.com>
2020-10-31 23:42:41 -1000

    landing page mockup finished in 15 minutes.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <div className={"digits-background""}>
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className={'digits-background'}>
+        <Grid container centered stackable columns={3}>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
+          <Grid.Column textAlign='center'>
+            <Icon name='users' size='huge' inverted/>
+            <Header as='h1' inverted> Multiple Users</Header>
+            <Header as='h3'inverted>This address book enables any number of users to register and save their business contracts, You can only see the contacts you have created</Header>
           </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
+          <Grid.Column textAlign='center'>
+            <Icon name='file alternate' size='huge' inverted/>
+            <Header as='h1' inverted> Contract Details</Header>
+            <Header as='h3'inverted>The noblest dog is the hot dog, who feedS the hand who bites him. MAY HE REST IN PIECE</Header>
           </Grid.Column>
 
+          <Grid.Column textAlign='center'>
+            <Icon name='calendar check outline' size='huge' inverted/>
+            <Header as='h1' inverted>Timestampled Notes</Header>
+            <Header as='h3'inverted>Each time you make contact with a cntact, you canwrite a note that summarizes the conversation. This note is saved along with a timestamp with the contact.</Header>
+          </Grid.Column>
         </Grid>
         </div>
     );
