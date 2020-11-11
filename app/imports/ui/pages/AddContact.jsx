c020a61bfc4ef6e3d220bc604500561062015672
Christian Pak <cpak2019@users.noreply.github.com>
2020-11-10 23:13:48 -1000

    Updated Undex

--- app/imports/ui/pages/AddContact.jsx
+++ app/imports/ui/pages/AddContact.jsx
@@ -43,7 +43,7 @@ class AddContact extends React.Component {
     return (
         <Grid container centered>
           <Grid.Column>
-            <Header as="h2" textAlign="center">Add Contace</Header>
+            <Header as="h2" textAlign="center">Add Contact</Header>
             <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
               <Segment>
                 <TextField name='firstName'/>
